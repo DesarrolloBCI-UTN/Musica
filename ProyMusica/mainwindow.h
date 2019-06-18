a63bc1fbd93bb5c6a091eb6cae957370851fece9
Germán Castro <6castrogerman@gmail.com>
2019-06-17 22:36:04 -0300

    Completo Template de Conexión
    TCP Server

--- ProyMusica/mainwindow.h
+++ ProyMusica/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
 
 namespace Ui {
 class MainWindow;
@@ -17,15 +19,35 @@ public:
 
 private slots:
     void on_BotonBombo_clicked();
-
     void on_BotonHiHat_clicked();
-
     void on_BotonRedoblante_clicked();
-
     void on_BotonCrash_clicked();
+
+    void on_btnConnect_clicked();
+    void on_btnListen_clicked();
+    void on_btnSend_clicked();
+    void on_btnClear_clicked();
+    void socket_aboutToClose();
+    void socket_bytesWritten(qint64 bytes);
+    void socket_connected();
+    void socket_disconnected();
+    void socket_error();
+    void socket_readyRead();
+    void socket_stateChanged();
+    void server_newConnection();
 
 private:
     Ui::MainWindow *ui;
+    bool connected;
+    QTcpServer server;
+    QTcpSocket *socket;
+
+private:
+    void log(QString msg);
+    void socket_events();
+    void update(bool connected);
+
+
 };
 
 #endif // MAINWINDOW_H
